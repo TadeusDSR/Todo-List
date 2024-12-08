b8af5930a75c5efa4f4282906be220fd5ade591e
TadeusDSR <127157247+TadeusDSR@users.noreply.github.com>
2024-12-08 17:22:55 +0100

    add form for adding tasks/projects

--- src/index.js
+++ src/index.js
@@ -4,14 +4,72 @@ const addBtn = document.getElementById("add-btn");
 const addMenu = document.getElementById("add-menu");
 const addProjectBtn = document.getElementById("add-project-btn");
 const addTaskBtn = document.getElementById("add-task-btn");
+const formContainer = document.getElementById("form-container");
 
 let isActive = 0;
+
 addBtn.addEventListener("click", () => {
-    if (isActive === 0) {
+    if (isActive == 0) {
         addMenu.style.display = "flex";
         isActive = 1;
     } else {
         addMenu.style.display = "none";
         isActive = 0;
     }
+});
+
+addProjectBtn.addEventListener("click", () => {
+    formContainer.style.display = "flex";
+    formContainer.innerHTML = 
+    `<form>
+
+      <div>
+        <label for="title">Name of Project</label>
+        <input type="text" name="title" id="title">
+      </div>
+
+      <div>
+        <button id="submit-btn" type="submit">
+          Add Project
+        </button>
+      </div>
+
+    </form>`;
+});
+
+addTaskBtn.addEventListener("click", () => {
+    formContainer.style.display = "flex";
+    formContainer.innerHTML = 
+    `<form>
+
+      <div>
+        <label for="title">Name of Task</label>
+        <input type="text" name="title" id="title">
+      </div>
+      <div>
+        <label for="desc">Description</label>
+        <input type="text" name="desc" id="desc">
+      </div>
+      <div>
+        <div>
+          <label for="due-date">Due Date</label>
+          <input type="date" name="due-date" id="due-date">
+        </div>
+        <div>
+          <label for="priority">Priority</label>
+          <select name="priority" id="priority">
+            <option value="high">high</option>
+            <option value="normal">normal</option>
+            <option value="low">low</option>
+          </select>
+        </div>
+      </div>
+
+      <div>
+        <button id="submit-btn" type="submit">
+          Add Task
+        </button>
+      </div>
+
+    </form>`;
 });
