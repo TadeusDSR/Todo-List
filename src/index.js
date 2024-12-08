788f0f49538b9b4ecf3293d7fbcbfb12f0d36d8b
TadeusDSR <127157247+TadeusDSR@users.noreply.github.com>
2024-12-08 14:33:40 +0100

    make add list button show menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+const addBtn = document.getElementById("add-btn");
+const addMenu = document.getElementById("add-menu");
+const addProjectBtn = document.getElementById("add-project-btn");
+const addTaskBtn = document.getElementById("add-task-btn");
+
+let isActive = 0;
+addBtn.addEventListener("click", () => {
+    if (isActive === 0) {
+        addMenu.style.display = "flex";
+        isActive = 1;
+    } else {
+        addMenu.style.display = "none";
+        isActive = 0;
+    }
+});
